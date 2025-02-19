7fc474fd7a0d30ad7576f9731c4890d19a63bf02
William Bodily <william.bodily@gmail.com>
2025-02-19 14:07:14 -0500

    Initial Commit

---
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinnerMenuApp.swift
+//  LittleLemonDinnerMenu
+//
+//  Created by William Bodily on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinnerMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
